42f52f1ea8ddb0543023f6dbf722ef7a01a052e7
Moritz Grünberg <gruenber@th-brandenburg.de>
2026-07-01 13:34:57 +0200

    Add previously removed demo code

---
+++ arduino-code/demo/demo.ino
@@ -0,0 +1,113 @@
+#include <Servo.h>
+
+const int analog = A0;
+const int digital = 2;
+const int R = 12;
+const int G = 13;
+const int servo = 9;
+const int buzzer = 3;
+
+Servo myservo;
+bool open = false;
+
+int val = 0;
+int messungen[50];
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(R, OUTPUT);
+  pinMode(G, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+  myservo.attach(servo);
+}
+
+void openFlower() {
+  if (!open) {
+    for(int i = 90; i >= -90; i -= 20) {
+      myservo.write(90-i);
+      delay(200);
+    }
+    open = true;
+  }
+  
+}
+
+void closeFlower() {
+  if (open) {
+    for(int i = -90; i <= 90; i += 20) {
+      myservo.write(90-i);
+      delay(200);
+    }
+    open = false;
+  }
+  
+}
+
+int dif(int messungen[]) {
+  int min = messungen[0];
+  int max = messungen[0];
+
+  for (int i = 1; i < 50; i++) {
+    if (messungen[i] > max) {
+      max = messungen[i];
+    } else if (messungen[i] < min) {
+      min = messungen[i];
+    }
+  }
+    return max-min;
+  
+}
+
+void checkDifs(int difs[]) {
+  int threshold = 10;
+  int counter = 0;
+  for (int i = 0; i < 10; i++) {
+    if (difs[i] >= threshold) {
+      counter += 1;
+    }
+  }
+  if (counter >= 4) {
+    digitalWrite(G, LOW);
+    digitalWrite(R, HIGH);
+    closeFlower();
+    delay(1000);
+  } else {
+    digitalWrite(G, HIGH);
+    digitalWrite(R, LOW);
+    tone(buzzer, 1000, 500);
+    delay(500);
+    openFlower();
+    delay(1000);
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  /*
+  int difs[10];
+  for(int i = 0; i < 10; i++) {
+    for(int i = 0; i < 50; i++) {
+      val = analogRead(analog);
+      //Serial.println(val);
+      messungen[i] = val;
+      delay(10);
+    }
+    int difference = dif(messungen);
+    Serial.println(difference);
+    difs[i] = difference;
+    checkDifs(difs);
+  }
+  */
+  digitalWrite(G, HIGH);
+  digitalWrite(R, LOW);
+  openFlower();
+  delay(3000);
+  digitalWrite(G, LOW);
+  digitalWrite(R, HIGH);
+  digitalWrite(buzzer, HIGH);
+  delay(500);
+  digitalWrite(buzzer, LOW);
+  closeFlower();
+  delay(3000);
+  
+}
